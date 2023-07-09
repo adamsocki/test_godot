58dda2353ceb9ba08cce43acc18363c4eaa24eee
Adam Socki <socki.adam@gmail.com>
2023-07-09 16:06:20 -0500

    motion started

--- game1_2D/player.gd
+++ game1_2D/player.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
 
+@export var speed = 4
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+#	position = pos
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("mouse_left"):
-		pass
+		velocity.x -= 1
+		
+		
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
 	
