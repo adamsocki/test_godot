7b24586c0f0d139a96dbada3b57f2ab11c8e27d9
Adam Socki <socki.adam@gmail.com>
2023-07-23 19:06:13 +0300

    init entity manager

---
+++ game1_2D/EntityManager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var entities = {}
+
+
+enum EntityType {TERRAIN, PLAYER, TEST, FERN, WALL, POST}
+
+func _ready():
+	# Initialize our entity storage
+	for type in EntityType.values():
+		entities[type] = []
+		
+
+func add_entity(type, entity):
+	# Add the entity to the appropriate list
+	entities[type].append(entity)
+	# And add it as a child of this node
+	add_child(entity)
+
+func delete_entity(type, entity):
+	# Remove the entity from the appropriate list
+	entities[type].erase(entity)
+	# And remove it as a child of this node
+	entity.queue_free()
+
+
