58fad88fd0b8bf50e33e11907110cf03e1056e89
Adam Socki <socki.adam@gmail.com>
2023-07-23 17:17:00 +0300

    relative path mod

--- game1_2D/wall.gd
+++ game1_2D/wall.gd
@@ -3,25 +3,55 @@ extends Node2D
 
 
 @export var door = false
+@export var locked = false
+
+
+#var root
+
+# Player Settings
+var player
+var in_range
+
+
 var text_box
 var root_node
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text_box = get_node("../../../UIManager/Camera_UI/ActionText")
-	text_box.text = "Press SPACE to Open Door"	
+	#root = get_tree().current_scene
+	
+	text_box = get_node("/root/GameManager/UIManager/Camera_UI/ActionText")
+
+	
+	
 	text_box.visible = false
+	in_range = false
+#	player = get_node("../../")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if door:
-		pass
-
+	if in_range:
+		pass 
 
 
 func _on_area_2d_body_entered(body):
-	text_box.visible = true
+	if door:
+		text_box.text = "Press SPACE to Open Door"	
+		
+		text_box.visible = true
+		in_range = true
+		
+		
 
 
 func _on_area_2d_body_exited(body):
-	text_box.visible = false
+	if door:
+		text_box.visible = false
+		in_range = false
+		
+
+
