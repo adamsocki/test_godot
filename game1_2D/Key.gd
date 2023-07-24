006e6527eae1ad105eb5f8040a9033540d9ac2dd
Adam Socki <socki.adam@gmail.com>
2023-07-24 13:10:42 +0300

    key trigger

--- game1_2D/Key.gd
+++ game1_2D/Key.gd
@@ -1,4 +1,30 @@
 # Key.gd
 extends "res://Entity.gd"
 
+
+var UI_Manager
+
+var pickup_label
+
+
+
 var key_code: String
+
+
+
+func _ready():
+	pickup_label = Label.new()
+	pickup_label.text = "Press X to pick up Key"
+	pickup_label.position = Vector2(400, 400)
+	pickup_label.visible = false
+	UI_Manager = get_node("/root/GameManager/UIManager/Camera_UI")
+	UI_Manager.add_child(pickup_label)
+	
+
+func _on_interact_player_body_entered(body):
+	pickup_label.visible = true
+	print(key_code)
+
+
+func _on_interact_player_body_exited(body):
+	pickup_label.visible = false
